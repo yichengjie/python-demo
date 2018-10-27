5dda8f7ec8831348b78a225dab2a887149298817
yichengjie <626659321@qq.com>
2018-10-27 12:16:22 +0800

    s

--- state3/statistic-group.py
+++ state3/statistic-group.py
@@ -4,28 +4,44 @@
 # find *.xml -exec sh -c "iconv -f GBK -t UTF8 {} > {}.xml" \;
 import xml.etree.ElementTree as ET
 
-filePath = r'./data/20180508_CA_BJS_yicj_265214_GroupImport.xml' 
-# 解析xml数据
-root = ET.parse(filePath)
-groups = root.getroot()
-#  根节点
-isCreate = groups.attrib['isCreate']
-#  group 的记录条数
-data = []
-for group in groups.findall('group'):
-	carrier_code = group.attrib['carrier_code']
-	location_code = group.attrib['location_code']
-	id = group.attrib['id']
-	groupdtls = group.findall('groupdtl')
-	data.append({'carrCode':carrier_code,'locCode':location_code,'id':id,'dtlCount':len(groupdtls)})
-	#print('carrCode : %s , locCode : %s , id : %s , dtlCount : %d' % (carrier_code,location_code,id,len(groupdtls)))
+class GroupXmlStatistic(object):
+	def __init__(self, filePath):
+		self.filePath = filePath
+		self.retData = []
 
-#  将数据展示出来
-dc = 0	
-for item in data:
-    dc += item['dtlCount']
-print('group count is : %d , dtl count is : %d ' % (len(data) ,dc))
-    		
+	def parseXml(self):
+		# 解析xml数据
+		root = ET.parse(self.filePath)
+		groups = root.getroot()
+		#  根节点
+		isCreate = groups.attrib['isCreate']
+		#  group 的记录条数
+		for group in groups.findall('group'):
+			carrier_code = group.attrib['carrier_code']
+			location_code = group.attrib['location_code']
+			id = group.attrib['id']
+			groupdtls = group.findall('groupdtl')
+			self.retData.append({'isCreate':isCreate,'carrCode':carrier_code,'locCode':location_code,'id':id,'dtlCount':len(groupdtls)})
+			#print('carrCode : %s , locCode : %s , id : %s , dtlCount : %d' % (carrier_code,location_code,id,len(groupdtls)))
+		return self.retData
+
+	def printXml(self):
+		#  将数据展示出来
+		dc = 0	
+		for item in self.retData:
+			dc += item['dtlCount']
+		print('group count is : %d , dtl count is : %d ' % (len(self.retData) ,dc))
+
+
+#  调用程序
+#  group文件路径
+xmlpath = r'./data/20180508_CA_BJS_yicj_265214_GroupImport.xml' 
+statistic = GroupXmlStatistic(xmlpath)
+#  解析xml文件并统计数据
+statistic.parseXml()	
+#  打印统计信息
+statistic.printXml()
+
 
 
 
