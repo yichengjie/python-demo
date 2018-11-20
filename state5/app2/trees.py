2422dba159c0dd5ffca11d135c1baa87623b284d
yichengjie <626659321@qq.com>
2018-11-20 22:19:01 +0800

    u

--- state5/app2/trees.py
+++ state5/app2/trees.py
@@ -1,4 +1,5 @@
 from math import log
+import operator
 
 def calcShannonEnt(dataSet):
 	numEntries = len(dataSet)
@@ -53,6 +54,26 @@ def chooseBestFeatureToSplit(dataSet):
 			bestInfoGain = infoGain
 			bestFeature = i
 	return bestFeature
+
+
+def majorityCnt(classList):
+	classCount = {}
+	for vote in classList:
+		if vote not in classList.keys():
+			classCount[vote] = 0
+		classCount[vote] += 1
+		sortedClassCount = sorted(classCount.items,key=operator.itemgetter(1), reverse=True)
+	return sortedClassCount[0][0]
+
+def createTree(dataSet,labels):
+	classList = [example[-1] for example in dataSet]
+	if classList.count(classList[0]) == len(classList):
+		return classList[0]
+	if len(dataSet[0]) == 1:
+		return majorityCnt(classList)
+
+	
+	pass
 
 
 
